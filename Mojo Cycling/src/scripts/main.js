6aa555ab155260baca9a7565deb5cf47c964a544
Ryan Bratton <tarrickryan@gmail.com>
2017-12-03 17:59:30 -0600

    mojo with sass

---
+++ Mojo Cycling/src/scripts/main.js
@@ -0,0 +1,21 @@
+
+
+  // $(function() {
+  //     var header = $(".nav-logo");
+  //     $(window).scroll(function() {
+  //         var scroll = $(window).scrollTop();
+  //
+  //         if (scroll >= 500) {
+  //             header.removeClass('nav-logo').addClass("small-brand");
+  //         } else {
+  //             header.removeClass("small-brand").addClass('nav-logo');
+  //         }
+  //     });
+  // });
+
+
+// BACK TO TOP BUTTON
+  function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
